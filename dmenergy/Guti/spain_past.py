a77fb8f1f906dabf9d1879e45114e45f6d0562e8
Gutai Dániel <kavics93@gmail.com>
2015-03-15 18:07:03 +0100

    Adatletöltés Spain
    Spanyolország áramfogyasztás és szélenergia termelés letöltése

---
+++ dmenergy/Guti/spain_past.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 21:16:33 2015
+
+@author: Dániel
+"""
+import time
+import csv
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from datetime import date
+from dateutil.rrule import rrule, DAILY
+
+sdate = date(2008, 1, 1)
+edate = date(2015,3,13)
+driver = webdriver.Chrome(r'C:\Users\Dániel\Downloads\WinPython-64bit-3.4.3.1\chromedriver')
+
+def Download(url,dest):
+    driver.get(url)
+    time.sleep(5)
+    file=open(dest,"w",newline='')
+    c=csv.writer(file,delimiter=',',quotechar=',', quoting=csv.QUOTE_MINIMAL)
+    for i in range(20,164):
+        if "Demand" in dest:
+            element = driver.find_element_by_xpath('//*[@id="tabla_evolucion"]/tbody/tr['+str(i)+']')
+        elif "Wind" in dest:
+            element = driver.find_element_by_xpath('//*[@id="tabla_generacion"]/tbody/tr['+str(i)+']')
+        html=element.get_attribute('innerHTML')
+        soup=BeautifulSoup(html)
+        html=soup.find_all('td')
+        a=html[0].text[:10]
+        b=html[0].text[11:16]
+        if "Demand" in dest:
+            c.writerow([a,b,html[1].text])
+        elif "Wind" in dest:
+            c.writerow([a,b,html[6].text])
+    file.close()
+
+for cdate in rrule(DAILY, dtstart=sdate, until=edate):
+    url1='https://demanda.ree.es/movil/peninsula/demanda/tablas/'+cdate.strftime("%Y-%m-%d")+'/1'
+    url2='https://demanda.ree.es/movil/peninsula/demanda/tablas/'+cdate.strftime("%Y-%m-%d")+'/2'
+    demand=r'D:\Suli\Önlab\Adatok\Spain\Demand\\'+cdate.strftime("%Y-%m-%d")+'.csv'
+    wind=r'D:\Suli\Önlab\Adatok\Spain\Wind\\'+cdate.strftime("%Y-%m-%d")+'.csv'
+    Download(url1,demand)
+    Download(url2,wind)
+driver.close()
+    
